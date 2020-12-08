603b36b4a09a3677a350635dd374bb9d83fc3192
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-08 13:18:00 +0000

    Modal 1 styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import axios from 'axios'
 import Navbar from './components/Navbar';
 
 function App() {
+  // Fetch data 
+
   return (
-    < >
+    <>
       <Navbar />
     </>
   );
